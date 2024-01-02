f3723a0be55f426a695e0bd850edae9d3113b875
IBRA956 <zibrahimsalih@gmail.com>
2024-01-02 13:35:43 -0800

    102-magic_calculation.py

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,5 +1,3 @@
+#!/usr/bin/python3
 def magic_calculation(a, b):
-        """Performs a calculation involving powers and addition."""
-            result = 98 + a ** b  # Matches the bytecode operations
-                return result
-
+        return 98 + a ** b
