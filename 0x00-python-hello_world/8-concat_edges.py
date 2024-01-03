1d88c8e5a5333812273aabd97301c8fcf94c656f
IBRA956 <zibrahimsalih@gmail.com>
2024-01-03 04:29:07 -0800

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
-
-# Define a string
 str = "Python is an interpreted, interactive, object-oriented programming\
-        language that combines remarkable power with very clear syntax"
-
-# Perform string slicing and concatenation to create a new string
-str = str[39:67] + str[107:112] + str[:6]
-
-# Print the resulting string
+ language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[106:112] + str[:6]
 print(str)
-
