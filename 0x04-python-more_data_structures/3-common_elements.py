48b25dbd4a577885c4d7403f3d18a54620c55940
IBRA956 <zibrahimsalih@gmail.com>
2024-01-10 08:32:53 -0800

    3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,9 +1,9 @@
+#!/usr/bin/python3
+
+
 def common_elements(set_1, set_2):
-    return set_1 & set_2
-
-# Example usage
-set_1 = {"Python", "C", "Javascript"}
-set_2 = {"Bash", "C", "Ruby", "Perl"}
-c_set = common_elements(set_1, set_2)
-print(sorted(list(c_set)))
-
+    """
+    A function that returns a set of
+    common elements in two sets
+    """
+    return(set_1 & set_2)
