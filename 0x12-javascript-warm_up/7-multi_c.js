aefd5f5cb12203b19900bfce2cc5d85dc6b17529
IBRA956 <zibrahimsalih@gmail.com>
2024-04-08 17:26:43 -0700

    all good

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,14 @@
-
-let printXt = parseInt(process.argv[2]);
-if (isNaN(printXt) || process.argv[2] === undefined) {
-  console.log('Missing number of occurences');
+if (process.argv.length < 3) {
+  console.log('Missing number of occurrences');
 } else {
-  while (printXt > 0) {
-    console.log('C is fun');
-    printXt--;
+  let printXt = parseInt(process.argv[2]);
+  if (isNaN(printXt)) {
+    console.log('Invalid number of occurrences');
+  } else {
+    while (printXt > 0) {
+      console.log('C is fun');
+      printXt--;
+    }
   }
 }
+
