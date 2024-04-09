0c0a54460cf6a94b169c3abc79e65ae4f70da2ba
IBRA956 <zibrahimsalih@gmail.com>
2024-04-08 17:33:02 -0700

    all good

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,4 +1,16 @@
-#!/usr/bin/node
-exports.addMeMaybe = function (number, theFunction) {
-  theFunction(number + 1);
-}
+def add_me_maybe(number, the_function):
+    """
+    Adds 1 to the given number and calls the provided function with the result.
+    
+    Args:
+        number (int): The input number.
+        the_function (function): The function to be called with the result.
+    """
+    the_function(number + 1)
+
+# Example usage
+def print_result(result):
+    print(f"Result: {result}")
+
+add_me_maybe(5, print_result)
+
