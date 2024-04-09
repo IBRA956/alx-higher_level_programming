5800beec0ac40093d3281b12ab3ba13806fe8643
IBRA956 <zibrahimsalih@gmail.com>
2024-04-08 17:47:24 -0700

    all good

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,18 +1,11 @@
-import sys
+#!/usr/bin/node
 
-def print_c_is_fun(num_occurrences):
-    if not num_occurrences.isdigit():
-        print("Missing number of occurrences")
-    else:
-        num_occurrences = int(num_occurrences)
-        while num_occurrences > 0:
-            print("C is fun")
-            num_occurrences -= 1
-
-# Get the command line argument (number of occurrences)
-if len(sys.argv) > 1:
-    num_occurrences = sys.argv[1]
-    print_c_is_fun(num_occurrences)
-else:
-    print("Missing number of occurrences")
-
+if (process.argv.length <= 2) {
+  console.log('Missing number of occurences');
+} else {
+  let i = 0;
+  const max = process.argv[2];
+  for (i; i < max; i++) {
+    console.log('C is fun');
+  }
+}
