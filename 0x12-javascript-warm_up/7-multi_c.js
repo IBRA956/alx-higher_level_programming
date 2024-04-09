4ada2392d96ab45a5789b3c0bee39ab8656b78dd
IBRA956 <zibrahimsalih@gmail.com>
2024-04-08 17:29:34 -0700

    all good

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,14 +1,18 @@
-if (process.argv.length < 3) {
-  console.log('Missing number of occurrences');
-} else {
-  let printXt = parseInt(process.argv[2]);
-  if (isNaN(printXt)) {
-    console.log('Invalid number of occurrences');
-  } else {
-    while (printXt > 0) {
-      console.log('C is fun');
-      printXt--;
-    }
-  }
-}
+import sys
+
+def print_c_is_fun(num_occurrences):
+    if not num_occurrences.isdigit():
+        print("Missing number of occurrences")
+    else:
+        num_occurrences = int(num_occurrences)
+        while num_occurrences > 0:
+            print("C is fun")
+            num_occurrences -= 1
+
+# Get the command line argument (number of occurrences)
+if len(sys.argv) > 1:
+    num_occurrences = sys.argv[1]
+    print_c_is_fun(num_occurrences)
+else:
+    print("Missing number of occurrences")
 
